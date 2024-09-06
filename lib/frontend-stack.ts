f4ea5b4b21145ffaca9a05361ea7137492240411
hunter4capture <hunter4capture@gmail.com>
2024-09-06 11:58:06 +0200

    updated wait to 2 min

--- lib/frontend-stack.ts
+++ lib/frontend-stack.ts
@@ -173,17 +173,6 @@ export class FrontendStack extends cdk.Stack {
       time: sfn.WaitTime.duration(cdk.Duration.seconds(10))
     });
 
-    // const incrementCounter = new sfn.Pass(scope, 'IncrementCounter', {
-    //   resultPath: '$.counter',
-    //   parameters: {
-    //     'counter.$': 'States.MathAdd($.counter, 1)'
-    //   }
-    // });
-
-    // const checkCounter = new sfn.Choice(scope, 'CheckCounter')
-    //   .when(sfn.Condition.numberGreaterThanEquals('$.counter', 10), endstate)
-    //   .otherwise(wait10s.next(checkConfirmationTask));
-
     // prettier-ignore
     const outboundChain = sfn.Chain.start(
       checkAgreementIdTask
@@ -204,7 +193,7 @@ export class FrontendStack extends cdk.Stack {
     const companyRegistrationStateMachine = new sfn.StateMachine(this, 'CompanyRegistrationStateMachine', {
       stateMachineName: 'CompanyRegistrationStateMachine',
       definitionBody: sfn.DefinitionBody.fromChainable(outboundChain),
-      timeout: cdk.Duration.seconds(30)
+      timeout: cdk.Duration.minutes(2)
     });
 
     //#endregion
